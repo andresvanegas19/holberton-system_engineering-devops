cff63eccb475e15e1f2a974069503612ed94babf
andres reyes <andressantiagore@gmail.com>
2020-08-03 22:05:17 +0000

    add the command to execute and end with the process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# Using Puppet, create a manifest that kills a process named killmenow.
+exec { 'kill_process':
+  command => '/usr/bin/pkill killmenow',
+  onlyif => '/usr/bin/test -e ./killmenow',
+}
