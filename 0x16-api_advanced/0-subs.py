b667ac0fb909fdc32f4fbf615cbed7cd92baa6b4
andres reyes <andressantiagore@gmail.com>
2020-09-17 15:40:57 -0500

    remove the conditionals and the arguments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,13 +15,11 @@ def number_of_subscribers(subreddit):
     }
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    r = requests.get(url=url, headers=headers, allow_redirects=False)
+    r = requests.get(url=url, headers=headers)
     if r.status_code == 302:
         return 0
     r = r.json()
     if r.get('kind') == 'Listing':
-        return 0
-    if r.get('error') is None:
         return 0
 
     return r.get('data').get('subscribers')
